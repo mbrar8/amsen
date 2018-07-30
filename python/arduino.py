1f04be80aa05662523fde837e8e4f080eadc89ff
Maheep <maheep@opas.ai>
2018-07-29 19:39:54 -0700

    July 2018 Changes Added functionality for multiple Arduino devices. Had to remove the while True loop when reading it, meaning have to return to it in case reading fails. Also modified pi.sh for the new static IP address 192.168.0.8. Also created a test.py to run sample code, currently for arduino reading and driver.

--- python/arduino.py
+++ python/arduino.py
@@ -1,52 +1,48 @@
 import time 
 import serial
+
+
 class Arduino:
-	def __init__(self):
-		self.data_in = serial.Serial('/dev/ttyACM0', 115200, timeout=2)
-                self.data_in.readline()
-                self.sensor_input = {}
+    def __init__(self, device):
+        self.device = device
+        self.data_in = serial.Serial(self.device, 115200, timeout=2)
+        self.data_in.readline()
+        self.sensor_input = {}
 
-	def read(self):
-		self.sensor_input={}
-            	while True:
-		  self.data_in.read(self.data_in.inWaiting())
-		  var = self.data_in.readline()
-		  try:
-		    parts = var.split(',')
-		    print parts
-		    for x in parts:
-			final = x.split('=')
-			self.sensor_input[final[0]] = float(final[1])
-                    # Check that compass and sonar value is present in sensor_input, if its missing than its garbage value we should ignore
-		    self.sensor_input['CMP']
-		    self.sensor_input['SNR']
-		    return
-		  except:
-		    print "ignoring garbgage"
+    def read(self):
+        self.sensor_input={}
+        self.data_in.read(self.data_in.inWaiting())
+        var = self.data_in.readline()
+        try:
+            parts = var.split(',')
+            print parts
+            for x in parts:
+                final = x.split('=')
+                self.sensor_input[final[0]] = float(final[1])
+            # Check that compass and sonar value present in sensor_input, if missing than it's garbage value to ignore
+            self.sensor_input['CMP']
+            self.sensor_input['SNR']
+            return
+        except:
+            print "ignoring garbage"
 
-	def measure_compass(self):
-		self.read()
-		return self.sensor_input['CMP']
+    def measure_compass(self):
+        self.read()
+        return self.sensor_input['CMP']
 
-	def measure_sonar(self):
-		self.read()
-		return self.sensor_input['SNR']
+    def measure_sonar(self):
+        self.read()
+        return self.sensor_input['SNR']
 
-        def compass(self):
-		return self.sensor_input['CMP']
+    def compass(self):
+        return self.sensor_input['CMP']
         
-        def sonar(self):
-		return self.sensor_input['SNR']
-	
-        def environment(self):
-		return self.sensor_input;
+    def sonar(self):
+        return self.sensor_input['SNR']
 
-'''
-'Test code
-a = Arduino()
-while True:
-  a.read()
-  print a.sensor_input
-  time.sleep(1)
-'''
+    def environment(self):
+        return self.sensor_input
+
+
+
 
--- python/pi.sh
+++ python/pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-host=192.168.86.192
+host=192.168.0.8
 scp ~/amsen/git/amsen/python/*.py pi@$host:/home/pi/Strnad/amsen/python
 scp ~/amsen/git/amsen/python/*.sh pi@$host:/home/pi/Strnad/amsen/python
 echo executing run.sh on raspberry pi
-ssh -tt pi@$host sudo /home/pi/Strnad/amsen/python/run.sh
+#ssh -tt pi@$host sudo /home/pi/Strnad/amsen/python/run.sh
